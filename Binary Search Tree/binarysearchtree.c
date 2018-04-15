a231feea3f4defcf142cb9c03fd94775c791e5af
35681242+mortonman <users.noreply.github.com>
2018-04-15 15:16:04 -0700

    added delete_value()

--- Binary Search Tree/binarysearchtree.c
+++ Binary Search Tree/binarysearchtree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 struct node{
 	int count;
@@ -52,7 +53,7 @@ void insert_value(struct node **head, int new_value){
 			if(move_right(&current_node) == false){
 				current_node->right = new_node;
 			}
-		}else{ //In the case of a duplicate valuetures
+		}else{ //In the case of a duplicate values
 			current_node->count++;
 			free(new_node);
 			return;
@@ -61,11 +62,125 @@ void insert_value(struct node **head, int new_value){
 	return;
 }
 
+void delete_value(struct node **head, int target_value){
+	//search for node with target value
+	struct node *current_node = *head;
+	struct node *parent_node = NULL;
+	//if there are no nodes in the tree
+	if(*head == NULL) return;
+	while(current_node->data!=target_value){
+		parent_node = current_node;	
+		if(target_value < current_node->data){ 
+			if(move_left(&current_node)==false){
+				return;
+			}
+		}else if(target_value > current_node->data){
+			if(move_right(&current_node)==false){
+				return;
+			}
+		}
+	}
+	if(current_node->count > 1){
+		current_node->count--;
+		return;
+	}
+	//if the node to be deleted has no children
+	if(current_node->right == NULL && current_node->left == NULL){
+		if(parent_node != NULL && parent_node->right == current_node){
+			parent_node->right = NULL;
+		}else if(parent_node != NULL){
+			parent_node->left = NULL;
+		}
+		free(current_node);
+		return;
+	}
+	//if the current node has a right child and no left child
+	if(current_node->right != NULL && current_node->left == NULL){
+		if(parent_node != NULL && parent_node->right == current_node){
+			parent_node->right = current_node->right;
+		}else if(parent_node != NULL){
+			parent_node->left = current_node->right;
+		}
+		free(current_node);
+		return;
+	}
+	//if the current child has a left child and no right child
+	if(current_node->left != NULL && current_node->right == NULL){
+		if(parent_node != NULL && parent_node->right == current_node){
+			parent_node->right = current_node->left;
+		}else if(parent_node != NULL){
+			parent_node->left = current_node->left;
+		}
+		free(current_node);
+		return;
+	}
+	//if the current node has two child nodes
+	if(current_node->left != NULL && current_node != NULL){
+		struct node *probe = current_node->right;
+		//finds the leftmost node.
+		while(probe->left != NULL){
+			parent_node = probe;
+			probe = probe->left;
+		}
+		current_node->data == probe->data;
+		if(probe->right != NULL){
+			if(parent_node != NULL && parent_node->right == probe){
+				parent_node->right = probe->right;
+			}else if(parent_node != NULL){
+				parent_node->left = probe->right;
+			}
+		}else{
+			if(parent_node != NULL && parent_node->right == probe){
+				parent_node->right = NULL;
+			}else if(parent_node != NULL){
+				parent_node->left = NULL;
+			}		
+		}
+		free(probe);
+		return;
+		
+
+	}
+
+}
+
+void print_tree(struct node *bst, int indent)
+{
+    for(int i = 0; i < indent; i++)
+    {
+                        printf( "\t");
+    }
+    fprintf(stdout,"%i\n", bst->data);
+    if(bst->left)
+    {
+                print_tree(bst->left, indent + 1);
+    }
+    if(bst->right)
+    {
+            print_tree(bst->right, indent + 1);
+    }
+}
+
 void main(){
-	struct node *tree = NULL;
-	insert_value(&tree, 16);
-	insert_value(&tree, 4);
-	insert_value(&tree, 20);
-	insert_value(&tree, 17);
-	insert_value(&tree, 17);
+	srand(time(NULL));
+	int values[20];
+	struct node *head = NULL;
+	for(int i = 0; i < 20; i++)
+	{
+		if(head != NULL){
+			print_tree(head, 0);
+		}
+		printf("\n\n---------------------\n");
+	    values[i] = rand() % 100;
+	    insert_value(&head, values[i]);
+	}
+	printf("deleting values:");
+	for(int i = 0; i < 20; i++)
+	{
+		if(head!=NULL){
+		print_tree(head, 0);
+		}
+		printf("\n\n-----------------------\n");
+		delete_value(&head, values[i]);
+	}
 }
